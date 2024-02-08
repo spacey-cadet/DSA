5aa33ef5213e276a8313a65812072a166c858c8a
TheoKorir <theo.korirchbng@gmail.com>
2024-02-08 12:43:14 +0300

    Create a hash table using c Print fuxntion for the hash table Free table and items functions

--- hashtable.cpp
+++ hashtable.cpp
@@ -1,25 +1,80 @@
-#include <iostream>
-#include <list>
-#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-using namespace std;
+#define CAPACITY 50000
 
-struct Node{
-    int key;
-    int value;
-    Node(int k, int v): key(k), value(v) {};
-};
+unsigned long hash_function(char *string){
+    unsigned long i =0;
+    for(int j=0; string[j];j++)
+        i+=string[j];
 
-class HashTable{
-    private:
-        vector <list<Node>> table;
-        int size;
+    return i % CAPACITY;
+}
 
-        size_t hash(int key){
-            return key % table.size();
+typedef struct HashTableItem{
+    char * key;
+    char * value;
+
+} HashTableItem;
+
+typedef struct HashTable{
+    HashTableItem ** items;
+    int count;
+    int size;
+
+} HashTable;
+
+HashTableItem * create_item(char* key, char* value){
+    HashTableItem* item = (HashTableItem*)malloc(sizeof(HashTableItem));
+    item->key=(char*)malloc(strlen(key)+1);
+    item->value= (char*)malloc(strlen(value)+1);
+    strcpy(item->key, key);
+    strcpy(item->value,value);
+    return item;
+}
+
+
+HashTable* create_table(int size){
+    HashTable* hashtable = (HashTable*)malloc(sizeof(HashTable));
+    hashtable->size= size;
+    hashtable->count = 0;
+    hashtable->items = (HashTableItem**)malloc(sizeof(HashTableItem*));
+    for(int i; i<hashtable->size; i++){
+        hashtable->items[i]=NULL;
+    }
+    return hashtable;
+}
+
+void free_item(HashTableItem* item){
+    free(item->key);
+    free(item->value);
+    free(item);
+}
+
+void free_item(HashTable* table){
+    for(int i; i< table->size; i++){
+        HashTableItem* item= table->items[i];
+        if(item != NULL){
+            free(item);
         }
-    public:
-        HashTable(int initial_size): size(initial_size){
-            table.resize(size);
+    }
+    free(table->items);
+    free(table);
+}
+
+void print_table(HashTable * table){
+    printf("\nHashTable\n------------------\n");
+    for(int i; i<table->size; i++){
+        if(table->items[i]){
+            printf("Index: %d, Key: %s, Value: %d \n", i, table->items[i], table->items[i]->value);
         }
-};
+    }
+    printf("------------------\n");
+}
+
+void insert(HashTable*  table, char* key, char* value){
+    
+
+}
+
